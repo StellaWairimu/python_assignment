c2b144f050d58abb09fe07726b12675740574d61
stella <stellawaihuini@gmail.com>
2022-10-23 23:00:44 +0000

    add odd numbers between 1,30

--- financialStatement.py
+++ financialStatement.py
@@ -3,3 +3,11 @@ for x in range(1,30):
     while x % 2 == 0:
         print(x)
         x+=1
+
+#Number 2. Print odd numbers between 1 and 30
+
+for x in range(1,20):
+    while x % 2 >=1:
+        print(x)
+        x+=1
+        
